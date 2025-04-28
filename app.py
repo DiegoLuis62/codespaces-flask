65e824e165844772bb88b2881ae48336507a5164
DiegoLuis62 <81648381+DiegoLuis62@users.noreply.github.com>
2025-04-28 23:40:42 +0000

    Flash- estado de prueba 1

--- app.py
+++ app.py
@@ -1,7 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
+import pickle
+from Utils.predictor import predecir_ataque_corazon
 
+# Cargar el modelo
+with open('modelo/modelo_entrenado.pkl', 'rb') as f:
+    modelo = pickle.load(f)
+
+# Definir columnas
+columnas_modelo = [
+    'Sex', 'AgeCategory', 'GeneralHealth', 'PhysicalHealthDays', 'MentalHealthDays', 'BMI',
+    'HadAngina', 'HadStroke', 'HadAsthma', 'HadCOPD', 'HadDiabetes',
+    'HadKidneyDisease', 'HadArthritis', 'SmokerStatus', 'ECigaretteUsage',
+    'AlcoholDrinkers', 'PhysicalActivities', 'SleepHours', 'HadDepressiveDisorder',
+    'HadSkinCancer', 'DifficultyWalking'
+]
+
+columnas_categoricas = [
+    'Sex', 'AgeCategory', 'GeneralHealth', 'HadAngina', 'HadStroke',
+    'HadAsthma', 'HadCOPD', 'HadDiabetes', 'HadKidneyDisease', 'HadArthritis',
+    'SmokerStatus', 'ECigaretteUsage', 'AlcoholDrinkers', 'PhysicalActivities',
+    'HadDepressiveDisorder', 'HadSkinCancer', 'DifficultyWalking'
+]
+
+# Crear app Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route('/')
+def home():
+    return "Servidor de predicción de enfermedades cardiacas funcionando 🚀"
+
+@app.route('/predecir', methods=['POST'])
+def predecir():
+    datos = request.get_json()
+
+    if not datos:
+        return jsonify({'error': 'No enviaste datos'}), 400
+
+    try:
+        prediccion, probabilidad = predecir_ataque_corazon(modelo, datos, columnas_modelo, columnas_categoricas)
+
+        return jsonify({
+            'resultado': 'Riesgo de Ataque' if prediccion == 'Yes' else 'Sin Riesgo',
+            'probabilidad': round(probabilidad * 100, 2)
+        })
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=6000, debug=True)
